95ab97b216a514fafe9be0d87bf6229490f1a9d6
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-05-12 20:09:30 +0530

    create inputbox with type script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import React, { useState } from "react";
+import { Todo } from "./models/models";
 
 const App: React.FC=()=> {
+  
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e : React.FormEvent ) =>{
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+interface props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<props> = ({ todo, setTodo,handleAdd}) => {
+    return (
+        <form className="input" onSubmit={(e) => {handleAdd(e)}}>
+            
+            <input type="text" placeholder="Enter a Task" value={todo} onChange={(e) => setTodo(e.target.value)} className="input__box" />
+            <button type="submit" className="input_submit">
+                GO
+            </button>
+        </form>
+    )
+}
+
+export default InputField
